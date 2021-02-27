f1a40539d6bd02cabf99ee22fa37234474ad8a99
Adeonir Kohl <adeonir@gmail.com>
2021-02-27 12:43:09 -0300

    Add completed challenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallenges.module.scss'
+
+export function CompletedChallenges() {
+  return (
+    <div className={styles.container}>
+      <span>Desafios completos</span>
+      <span>00</span>
+    </div>
+  )
+}
