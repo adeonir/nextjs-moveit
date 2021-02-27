d97df5235c24197068be583c3294f503a0bbdc2e
Adeonir Kohl <adeonir@gmail.com>
2021-02-27 11:54:13 -0300

    Add experience-bar component

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/ExperienceBar.module.scss'
+
+export function ExperienceBar() {
+  return (
+    <header className={styles.container}>
+      <span>0 xp</span>
+      <div>
+        <div className={styles.progress} style={{ width: '60%' }} />
+        <span className={styles.current} style={{ left: '60%' }}>400px</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
+
+export default ExperienceBar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import ExperienceBar from '../components/ExperienceBar'
+
 import styles from '../styles/pages/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello, world!</h1>
+      <ExperienceBar />
     </div>
   )
 }
